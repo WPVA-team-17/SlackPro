e0426bc6f0fadd21c4ba780cbae0baa58b7b24cc
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2022-10-23 23:43:49 +0200

    Change MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,49 +1,54 @@
 <template>
-  <q-layout view="lHh Lpr lFf" ref="ScrollContainer">
-    <q-infinite-scroll :offset="250" reverse class="q-pa-md q-pb-xl" ref="scroll">
-      <q-chat-message v-for="item in items" :key="item.id"
-        :name=item.name
-        :text=[item.text]
-        :sent="item.sent"
-        :stamp="item.stamp"
-      />
-    </q-infinite-scroll>
-    <q-footer bordered elevated dark>
-      <q-form @submit="onSubmit" class="q-gutter-md" ref="bottomScroll">
-        <my-input/>
-      </q-form>
-    </q-footer >
+  <q-layout view="hHh Lpr fFf" ref="ScrollContainer">
+
+    <q-drawer show-if-above side="left" bordered elevated >
+      <chats-list />
+    </q-drawer>
+
+    <q-page-container>
+      <channel-conversation ref="conversation"/>
+    </q-page-container>
+
+    <q-footer bordered elevated class="bg-green-10">
+        <main-input @cliInput="onCLIinput" class="cli"/>
+    </q-footer>
+
   </q-layout>
 </template>
 
-<script lang="js">
-import { defineComponent } from 'vue'
-import MyInput from './MyInput.vue'
-import { messagesStore } from '../stores/example-store'
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import ChatsList from 'src/components/ChatsList.vue'
+import MainInput from '../components/MainInput.vue'
+import ChannelConversation from '../components/ChannelConversation.vue'
 
 export default defineComponent({
   name: 'MainLayout',
 
   components: {
-    MyInput
+    ChatsList,
+    MainInput,
+    ChannelConversation
   },
 
   setup () {
-    const messages = messagesStore()
     return {
-      items: messages.messages,
-      messageStore: messages
+      conversation: ref(null)
     }
   },
   methods: {
-    async onSubmit (evt) {
-      const { value } = evt.target.elements[0]
-      await this.messageStore.addMessage(value)
-      window.scrollBy(0, 50000000000)
+    async onCLIinput (messsage: string) {
+      await (this.$refs.conversation as any).addMessage(messsage)
+      window.scrollBy(0, 1000)
     }
   }
 })
 </script>
 
 <style scoped lang="css">
+.cli{
+  width: 60%;
+  margin-left: auto;
+  margin-right: auto;
+}
 </style>
